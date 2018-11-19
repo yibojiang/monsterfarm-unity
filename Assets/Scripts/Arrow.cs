decf19078c5ad239a92b3c7641563aabe145e9d1
yibojiang <yibojiang00@gmail.com>
2018-11-18 20:13:28 -0500

    add arrow

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+	private Vector3 movingVel_;
+	private float lifeTime_ = 0.5f;
+	private float life_;
+	private bool hasShoot_ = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hasShoot_) {
+			life_ += Time.deltaTime;
+			if (life_ > lifeTime_) {
+				Destroy(this.gameObject);
+			}
+		}
+	}
+
+	public void Shoot(Vector3 movingVel) {
+		hasShoot_ = true;
+		movingVel_ = movingVel;
+		movingVel_.z = 0;
+		movingVel_.Normalize();
+	}
+
+	void FixedUpdate () {
+
+		transform.position = transform.position + movingVel_ * Time.fixedDeltaTime * 20f;
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -50,7 +50,10 @@ public class PlayerController : MonoBehaviour {
 	private Camera cam;
 
 	private int coins_;
-	private bool isAiming = false;
+	private bool isAiming_ = false;
+
+	public Transform shootTarget;
+	private GameObject arrow_;
 
 	public int Coins {
 		get {
@@ -123,6 +126,26 @@ public class PlayerController : MonoBehaviour {
 			}
 		}
 
+		if (Input.GetMouseButton(1)) {
+			if (!isAiming_) {
+				AimStart();
+			}
+			isAiming_ = true;
+
+			if (Input.GetMouseButtonUp(0)) {
+				animSM_.SetTrigger("Shoot");
+				Shoot();
+			}
+		}
+		else {
+			if (arrow_) {
+				Destroy(arrow_);
+			}
+			isAiming_ = false;
+		}
+
+		animSM_.SetBool("IsAiming", isAiming_);
+
 		if (interactTarget!= null) {
 			textInteract.gameObject.SetActive(true);
 		}
@@ -136,32 +159,70 @@ public class PlayerController : MonoBehaviour {
 
 		// }
 	}
+
+	void AimStart() {
+		var arrowPrefab = Resources.Load("Prefab/Arrow");
+		arrow_ = (GameObject)GameObject.Instantiate(arrowPrefab, shootTarget.position, Quaternion.identity);
+		arrow_.transform.parent = shootTarget;
+		arrow_.transform.localRotation = Quaternion.identity;
+	}
 	
+	void Shoot() {
+		arrow_.transform.parent = null;
+		var mousePos = Input.mousePosition;
+		var pos = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+		var shootingDir = pos - shootTarget.position;
+		arrow_.GetComponent<Arrow>().Shoot(shootingDir);
+		arrow_ = null;
+		isAiming_ = false;
+		// var arrowPrefab = Resources.Load("Prefab/Arrow");
+		// var arrow = (GameObject)GameObject.Instantiate(arrowPrefab, shootTarget.position, Quaternion.identity);
+		// arrow.transform.parent = shootTarget;
+	}
 	// Update is called once per frame
 	void FixedUpdate () {
-		var playerInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
-		
-		playerInput = Vector3.Normalize(playerInput);
+		if (!isAiming_) {
+			var playerInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+			playerInput = Vector3.Normalize(playerInput);
 
-		movingVel += playerInput * acc;
+			movingVel += playerInput * acc;
 
-		if (movingVel.magnitude > 0.1) {
-			movingVel -= movingVel * drag;
-			movingVel = Vector3.ClampMagnitude(movingVel, maxMovingSpeed);
-			lastMovingVel_ = movingVel;
+			if (movingVel.magnitude > 0.1) {
+				movingVel -= movingVel * drag;
+				movingVel = Vector3.ClampMagnitude(movingVel, maxMovingSpeed);
+				lastMovingVel_ = movingVel;
+			}
+			else {
+				movingVel = Vector3.zero;
+			}
+			// Debug.Log(movingVel);
+
+			transform.position = transform.position + movingVel * Time.fixedDeltaTime;
+			animSM_.SetFloat("Speed", movingVel.magnitude);
+			// var direction = (Mathf.Atan2(movingVel.y, movingVel.x));
+			animSM_.SetFloat("DirectionX", lastMovingVel_.x);
+			animSM_.SetFloat("DirectionY", lastMovingVel_.y);
 		}
 		else {
-			movingVel = Vector3.zero;
+			var mousePos = Input.mousePosition;
+			var pos = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+			var shootingDir = pos - shootTarget.position;
+			Vector3.Normalize(shootingDir);
+				// curBlueprint.transform.position = pos - new Vector3(0f, 0f, 0f);
+				// if (Input.GetMouseButton(0)) {
+				// 	var bp = curBlueprint.GetComponent<BuildBlueprint>();
+				// 	bp.EnableCollider();
+				// 	Destroy(bp);
+				// 	curBlueprint = null;
+				// }
+			// }
+			animSM_.SetFloat("ShootingDirectionX", shootingDir.x);
+			animSM_.SetFloat("ShootingDirectionY", shootingDir.y);
+			var eulerAngles = shootTarget.transform.localEulerAngles;
+			eulerAngles.z = Mathf.Atan2(shootingDir.y, shootingDir.x) * Mathf.Rad2Deg;
+			shootTarget.transform.localEulerAngles = eulerAngles;
 		}
-		// Debug.Log(movingVel);
-
-		transform.position = transform.position + movingVel * Time.fixedDeltaTime;
-		animSM_.SetFloat("Speed", movingVel.magnitude);
-		// var direction = (Mathf.Atan2(movingVel.y, movingVel.x));
 		
-		
-		animSM_.SetFloat("DirectionX", lastMovingVel_.x);
-		animSM_.SetFloat("DirectionY", lastMovingVel_.y);
 		// Debug.Log(direction);
 		// Debug.Log(movingVel.magnitude);
 		// if (movingVel.x > 0)

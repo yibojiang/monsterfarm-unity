c274328265f3a2162cfdaa839d314303cb34a620
yibojiang <yibojiang00@gmail.com>
2019-02-10 23:42:49 -0500

    fix monster collision issue

---
+++ Assets/Scripts/InteractFoodbow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractFoodbow : InteractItem {
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    public override void InteractAction()
+    {
+        Debug.Log("InteractAction");
+        var pc = PlayerController.Instance;
+        if (pc.items.ContainsKey(itemName) && pc.items[itemName] + itemCount >= 0)
+        {
+            Debug.Log("InteractAction2");
+            pc.items[itemName]+=itemCount;
+        }
+        else
+        {
+            var allTexts = new List<string>();
+            var dm = DialogManager.Instance;
+            allTexts.Add(string.Format("I don't have any {0}.", itemName));
+            dm.SetText(allTexts);
+            dm.Show();
+        }
+    }
+}
--- Assets/Scripts/InteractNPC.cs
+++ Assets/Scripts/InteractNPC.cs
@@ -19,7 +19,6 @@ public class InteractNPC : Interact {
 		var dm = DialogManager.Instance;
 		var allTexts = new List<string>();
 		
-
 		var pc = PlayerController.Instance;
 
 		if (pc.items.ContainsKey("egg") && pc.items["egg"] > 0) {

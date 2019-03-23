1c3b4ea56b6bdd7f8a9540fee992cfbdbf82b149
yibojiang <yibojiang00@gmail.com>
2019-03-23 16:09:37 -0400

    fix prefab naming, remove draw order

--- Assets/Scripts/UpdateDrawOrder.cs
+++ Assets/Scripts/UpdateDrawOrder.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [ExecuteInEditMode]
 public class UpdateDrawOrder : MonoBehaviour {
 	private SpriteRenderer sprite_;
 	public int offset = 0;
+	
+	[MenuItem("Tools/DestroyAllDrawOrder")]
+	static void DoSomething()
+	{
+		var drawOrders = GameObject.FindObjectsOfType<UpdateDrawOrder>();
+		foreach (var drawCom in drawOrders)
+		{
+			DestroyImmediate(drawCom);
+		}
+	}
 	// Use this for initialization
 	void Start () {
 		sprite_ = this.gameObject.GetComponent<SpriteRenderer>();
